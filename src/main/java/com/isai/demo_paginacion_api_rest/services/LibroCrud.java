93726a2f56187a4518f39e0434e255212028db7b
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-10 11:37:56 -0500

    definicion de metodo crud para listar los libros

---
+++ src/main/java/com/isai/demo_paginacion_api_rest/services/LibroCrud.java
@@ -0,0 +1,11 @@
+package com.isai.demo_paginacion_api_rest.services;
+
+import java.util.List;
+
+import com.isai.demo_paginacion_api_rest.models.Libro;
+
+public interface LibroCrud {
+
+    List<Libro> listarLibros();
+
+}
