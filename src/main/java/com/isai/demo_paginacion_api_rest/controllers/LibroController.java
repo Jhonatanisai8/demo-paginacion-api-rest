4bd82f965db25df54aa5b93c8e34f1514602353b
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-10 12:20:14 -0500

    paginacion por default

--- src/main/java/com/isai/demo_paginacion_api_rest/controllers/LibroController.java
+++ src/main/java/com/isai/demo_paginacion_api_rest/controllers/LibroController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -45,4 +46,11 @@ public class LibroController {
     public Page<Libro> listarLibrosPaginadosOrdenadosParametros(Pageable pageable) {
         return libroService.listarLibrosPaginados(pageable);
     }
+
+    @GetMapping(path = "/paginados/ordenados/parametros/default")
+    public Page<Libro> librosPaginadosDefault(
+            @PageableDefault(page = 0, size = 10) Pageable pageable) {
+        return libroService.listarLibrosPaginados(pageable);
+    }
+
 }
