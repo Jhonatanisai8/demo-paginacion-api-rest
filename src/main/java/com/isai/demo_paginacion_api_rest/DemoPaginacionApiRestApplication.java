ec0f56daebe3128446b15882c12d840f65ca49ad
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-10 11:33:23 -0500

    init

---
+++ src/main/java/com/isai/demo_paginacion_api_rest/DemoPaginacionApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.isai.demo_paginacion_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPaginacionApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPaginacionApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isai/demo_paginacion_api_rest/DemoPaginacionApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.demo_paginacion_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPaginacionApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
