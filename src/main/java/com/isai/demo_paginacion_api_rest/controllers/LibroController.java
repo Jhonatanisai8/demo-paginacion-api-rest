6241405b292067ed6f54a232d5d9277111bc866e
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-10 12:11:35 -0500

    paginacion ordenada

--- src/main/java/com/isai/demo_paginacion_api_rest/controllers/LibroController.java
+++ src/main/java/com/isai/demo_paginacion_api_rest/controllers/LibroController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,6 +32,12 @@ public class LibroController {
     @GetMapping(path = "/paginados")
     public Page<Libro> listarLibrosPaginados() {
         final Pageable pageable = PageRequest.of(0, 10);
+        return libroService.listarLibrosPaginados(pageable);
+    }
+
+    @GetMapping(path = "/paginados/ordenados")
+    public Page<Libro> listarLibrosPaginadosOrdenados() {
+        final Pageable pageable = PageRequest.of(0, 10,Sort.by(Direction.DESC,"titulo"));
         return libroService.listarLibrosPaginados(pageable);
     }
 }
